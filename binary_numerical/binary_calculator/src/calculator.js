0e5d494259fc63e3cfd7ab1d4b45f138daee70e7
David Walton <davidwalton6772@gmail.com>
2020-10-01 14:23:55 -0500

    binary calculator function working

--- binary_numerical/binary_calculator/src/calculator.js
+++ binary_numerical/binary_calculator/src/calculator.js
@@ -1,72 +1,64 @@
-function binaryToDecimal(binary) {
-    const binaryArray = binary.toString().split("").map(Number);
-    const reverseBinary = binaryArray.reverse();
-    console.log(reverseBinary);
-    let result = 0;
-    for (let i = 0; i < reverseBinary.length; i++) {
+import React, { Component } from 'react'
 
-        switch (i) {
-            case 0:
-                console.log(result);
-                if (reverseBinary[i] != 0) {
-                    result += 1;
-                    console.log("position 0 =", result);
-                }
-                break;
-            case 1:
-                console.log(result);
-                if (reverseBinary[i] != 0) {
-                    result += 2;
-                    console.log("position 1 =", result);
-                }
-                break;
-            case 2:
-                console.log(result);
-                if (reverseBinary[i] != 0) {
-                    result += 4;
-                    console.log("position 2 =", result);
-                }
-                break;
-            case 3:
-                console.log(result);
-                if (reverseBinary[i] != 0) {
-                    result += 8;
-                    console.log("position 3 =", result);
-                }
-                break;
-            case 4:
-                console.log(result);
-                if (reverseBinary[i] != 0) {
-                    result += 16;
-                    console.log("position 4 =", result);
-                }
-                break;
-            case 5:
-                console.log(result);
-                if (reverseBinary[i] != 0) {
-                    result += 32;
-                    console.log("position 5 =", result);
-                }
-                break;
-            case 6:
-                console.log(result);
-                if (reverseBinary[i] != 0) {
-                    result += 64;
-                    console.log("position 6 =", result);
-                }
-                break;
-            case 7:
-                console.log(result);
-                if (reverseBinary[i] != 0) {
-                    result += 128;
-                    console.log("position 7 =", result);
-                }
-                break;
-            default:
-                console.log(`Error calculating decimal number.`);
+export default class calculator extends Component {
+
+    state = {
+        number: 0
+    };
+
+
+    // handleChange = (event) => {
+    //     let num = event.target.value;
+    //     console.log(num);
+    //     return num;
+    // }
+    onInputChange = (input) => (event) => {
+        this.setState({
+            [input]: (event.target.value),
+        }, () => { console.log(this.state) });
+    };// end onInputChange
+
+
+    binaryCalc = (num) => {
+        console.log("this state number", parseInt(this.state.number));
+        num = parseInt(this.state.number);
+        console.log(num);
+        let binNum = num.toString().split('');
+        console.log(binNum);
+        console.log(binNum);
+        let position = null;
+        let result = null;
+        let digit = null;
+        for (let i in binNum) {
+            position = binNum.length - 1 - i;
+            digit = binNum[i];
+            result += digit * 2 ** position;
+            console.log(
+                "Current position:",
+                position + " Current digit:",
+                digit + " Current result:",
+                result
+            );
         }
-        console.log(result);
+        console.log("Binary Calculator: ", result);
+
+        return result;
+    }
+
+
+
+
+
+    render() {
+        const answer = this.binaryCalc();
+        return (
+            <div>
+
+                <input onChange={this.onInputChange('number')}></input>
+                <button onClick={this.binaryCalc}>Click to Calculate</button>
+                <h1>{answer}</h1>
+
+            </div>
+        )
     }
 }
-
-binaryToDecimal(100101);
